f199145a7086520ff1e9885075bbed373594066c
Asukii <asukii314.twitch@gmail.com>
2023-08-03 16:02:15 -0400

    add detailed info on neow option chosen

--- src/main/java/SpireBot/info/InfoFinder.java
+++ src/main/java/SpireBot/info/InfoFinder.java
@@ -2,13 +2,18 @@ package SpireBot.info;
 
 import basemod.BaseMod;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
+import com.megacrit.cardcrawl.helpers.PotionHelper;
+import com.megacrit.cardcrawl.helpers.RelicLibrary;
 import com.megacrit.cardcrawl.helpers.SeedHelper;
+import com.megacrit.cardcrawl.localization.PotionStrings;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.TreeMap;
 
 public class InfoFinder {
@@ -145,11 +150,11 @@ public class InfoFinder {
                     continue;
 
                 if (k.containsKey("picked")) {
-                    String picked = k.get("picked").toString();
+                    String picked = RelicLibrary.getRelic(k.get("picked").toString()).name;
 
-                    sb.append("At the end of act " + act++ + ", we chose " + picked + " over " + notPicked.get(0) + " and " + notPicked.get(1) + ". ");
+                    sb.append("At the end of act " + act++ + ", we chose " + picked + " over " + RelicLibrary.getRelic(notPicked.get(0)).name + " and " + RelicLibrary.getRelic(notPicked.get(1)).name + ". ");
                 } else if (notPicked.size() == 3) {
-                    sb.append("At the end of act " + act++ + ", we took nothing and skipped " + notPicked.get(0) + ", " + notPicked.get(1) + ", and " + notPicked.get(2) + ". ");
+                    sb.append("At the end of act " + act++ + ", we took nothing and skipped " + RelicLibrary.getRelic(notPicked.get(0)).name + ", " + RelicLibrary.getRelic(notPicked.get(1)).name + ", and " + RelicLibrary.getRelic(notPicked.get(2)).name + ". ");
                 }
             }
         }
@@ -227,7 +232,7 @@ public class InfoFinder {
         StringBuilder sb = new StringBuilder();
         sb.append("We chose: ");
         sb.append(formatNeowInfo(CardCrawlGame.metricData.neowBonus, CardCrawlGame.metricData.neowCost));
-
+        sb.append(formatNeowInfoDetail(CardCrawlGame.metricData.neowBonus, CardCrawlGame.metricData.neowCost));
         JsonArray bonusesSkipped = BaseMod.getSaveFields().get("NeowBonusesSkippedLog").onSaveRaw().getAsJsonArray();
         JsonArray costsSkipped = BaseMod.getSaveFields().get("NeowCostsSkippedLog").onSaveRaw().getAsJsonArray();
 
@@ -278,15 +283,167 @@ public class InfoFinder {
             return "No neow option picked yet.";
         }
 
-       // strip enclosing quotation marks from skipped log strings
-       String neowBonus = neow_bonuses.get(neowBonusRaw.replace("\"", ""));
-       String neowCost = neow_costs.get(neowCostRaw.replace("\"", ""));
+       String neowBonus = neow_bonuses.get(clean(neowBonusRaw));
+       String neowCost = neow_costs.get(clean(neowCostRaw));
 
        if (neowCost == null) {
             return neowBonus;
         } else {
             return neowCost + " " + neowBonus;
         }
+    }
+
+    private static String clean(String s){
+        // sometimes strings will appear with quotation marks around them, other times they won't
+        // normalize here so keys are consistent
+        return s.replace("\"", "");
+    }
+
+    private static String formatNeowInfoDetail(String neowBonusRaw, String neowCostRaw){
+        StringBuilder sb = new StringBuilder();
+        sb.append(" ");
+        JsonObject bonusData = BaseMod.getSaveFields().get("NeowBonusLog").onSaveRaw().getAsJsonObject();
+        JsonArray cardList = bonusData.get("cardsObtained").getAsJsonArray();
+
+        if(Objects.equals(neowCostRaw, "CURSE")) {
+            sb.append("We got ");
+            sb.append(CardLibrary.getCard(clean(cardList.get(0).toString())));
+            sb.append(". ");
+        }
+
+        switch(neowBonusRaw){
+            case "TEN_PERCENT_HP_BONUS":
+            case "TWENTY_PERCENT_HP_BONUS":
+            case "THREE_ENEMY_KILL":
+            case "HUNDRED_GOLD":
+            case "TWO_FIFTY_GOLD":
+                if(Objects.equals(neowCostRaw, "CURSE")) {
+                    return sb.toString();
+                }
+                return "";
+
+            case "THREE_SMALL_POTIONS":
+                ArrayList<String> potions = new ArrayList();
+                ArrayList<HashMap> allPotionData = CardCrawlGame.metricData.potions_obtained;
+                for(int i = 0; i < allPotionData.size(); i++) {
+                    if((Integer) allPotionData.get(i).get("floor") == 0) {
+                        potions.add(allPotionData.get(i).get("key").toString());
+                    }
+                }
+                if(potions.size() == 0) {
+                    sb.append("We didn't take any of the three potions");
+                    break;
+                }
+
+                sb.append("We obtained ");
+                sb.append(PotionHelper.getPotion(potions.get(0)).name);
+
+                if(potions.size() == 2) {
+                    sb.append(" and ");
+                    sb.append(PotionHelper.getPotion(potions.get(1)).name);
+                } else if(potions.size() == 3) {
+                    sb.append(", ");
+                    sb.append(PotionHelper.getPotion(potions.get(1)).name);
+                    sb.append(", and ");
+                    sb.append(PotionHelper.getPotion(potions.get(2)).name);
+                }
+                break;
+
+            case "THREE_CARDS":
+            case "THREE_RARE_CARDS":
+            case "RANDOM_COLORLESS":    // choose uncommon colorless
+            case "RANDOM_COLORLESS_2":  // choose rare colorless
+                ArrayList<HashMap> cardChoices = CardCrawlGame.metricData.card_choices;
+                if(cardChoices.size() > 0) {
+                    HashMap firstCardChoice = cardChoices.get(0);
+                    if ((Integer) firstCardChoice.get("floor") == 0) {
+                        String picked = firstCardChoice.get("picked").toString();
+                        ArrayList<String> notPicked = (ArrayList<String>) firstCardChoice.get("not_picked");
+
+                        // skip all
+                        if(Objects.equals(picked, "SKIP") || (notPicked != null && notPicked.size() == 3)) {
+                            sb.append("We were offered ");
+                            sb.append(CardLibrary.getCard(notPicked.get(0)).name);
+                            sb.append(", ");
+                            sb.append(CardLibrary.getCard(notPicked.get(1)).name);
+                            sb.append(", and ");
+                            sb.append(CardLibrary.getCard(notPicked.get(2)).name);
+                            sb.append(", but skipped them all.");
+                            break;
+                        }
+
+                        // choose one
+                        sb.append("We picked ");
+                        sb.append(CardLibrary.getCard(picked).name);
+                        if (notPicked != null && notPicked.size() == 2) {
+                            sb.append(" over ");
+                            sb.append(CardLibrary.getCard(notPicked.get(0)).name);
+                            sb.append(" and ");
+                            sb.append(CardLibrary.getCard(notPicked.get(1)).name);
+                        }
+                        break;
+                    }
+                }
+                sb.append("We didn't pick any of the three cards offered");
+                break;
+
+            case "RANDOM_COMMON_RELIC":
+            case "ONE_RARE_RELIC":
+            case "BOSS_RELIC":   // TODO: see if there's transform stats for pbox/astrolabe
+                sb.append("We got ");
+                JsonArray relicList = bonusData.get("relicsObtained").getAsJsonArray();
+                sb.append(RelicLibrary.getRelic(clean(relicList.get(0).toString())));
+
+                break;
+
+            case "TRANSFORM_CARD":
+            case "TRANSFORM_TWO_CARDS":
+                sb.append("We transformed ");
+                JsonArray transformedList = bonusData.get("cardsTransformed").getAsJsonArray();
+                sb.append(CardLibrary.getCard(clean(transformedList.get(0).toString())));
+                if(transformedList.size() == 2) {
+                    sb.append(" and ");
+                    sb.append(CardLibrary.getCard(clean(transformedList.get(1).toString())));
+                }
+                sb.append(" into ");
+                if(Objects.equals(neowCostRaw, "CURSE")) {
+                    sb.append(CardLibrary.getCard(clean(cardList.get(1).toString())));
+                    sb.append(" and ");
+                    sb.append(CardLibrary.getCard(clean(cardList.get(2).toString())));
+                } else {
+                    sb.append(CardLibrary.getCard(clean(cardList.get(0).toString())));
+                    if (cardList.size() == 2) {
+                        sb.append(" and ");
+                        sb.append(CardLibrary.getCard(clean(cardList.get(1).toString())));
+                    }
+                }
+                break;
+
+            case "ONE_RANDOM_RARE_CARD":
+                sb.append("We got ");
+                sb.append(CardLibrary.getCard(clean(cardList.get(0).toString())));
+                break;
+
+            case "REMOVE_CARD":
+            case "REMOVE_TWO":
+                sb.append("We removed ");
+                JsonArray removedList = bonusData.get("cardsRemoved").getAsJsonArray();
+                sb.append(CardLibrary.getCard(clean(removedList.get(0).toString())));
+                if(removedList.size() == 2) {
+                    sb.append(" and ");
+                    sb.append(CardLibrary.getCard(clean(removedList.get(1).toString())));
+                }
+                break;
+
+            case "UPGRADE_CARD":
+                sb.append("We upgraded ");
+                JsonArray upgradeList = bonusData.get("cardsUpgraded").getAsJsonArray();
+                sb.append(CardLibrary.getCard(clean(upgradeList.get(0).toString())));
+                break;
+
+        }
+        sb.append(".");
+        return sb.toString();
     }
 
     public static String bossInfo() {
